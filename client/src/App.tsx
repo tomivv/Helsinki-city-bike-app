1d0a84a7b288a59ff7650d863b883ca1b39c2787
Tomi Niiranen <tominiiranen@MacBook-Pro>
2022-08-03 21:16:48 +0300

    Added react-router-dom package to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Stations from './pages/stations/Stations';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import View from './pages/journey/View';
 
 function App() {
   return (
     <div className="App">
-      <Stations />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/stations' element={<Stations />} />
+        <Route path='/journeys' element={<View />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
